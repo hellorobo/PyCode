50422315b91be222179c090d175da7e22d307871
hellorobo <trebor.oknewokaj@gmail.com>
2017-12-28 09:57:38 +0100

    first update

---
+++ dictionaries.py
@@ -0,0 +1,15 @@
+def translate_weekday(day):
+    week_days = {
+        'Monday'    : 'Poniedzialek',
+        'Tuesday'   : 'Wtorek',
+        'Wednesday' : 'Sroda',
+        'Thursday'  : 'Czwartek',
+        'Friday'    : 'Piatek',
+        'Saturday'  : 'Sobota',
+        'Sunday'    : 'Niedziela'
+    }
+    if day in week_days.keys():
+        print(week_days.get(day))
+
+
+translate_weekday('Sunday')
